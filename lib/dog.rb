ffe614e8fbb5a7f7514d1dc495457218f3458abb
Sarah Paz <sarah.paz@gmail.com>
2019-01-03 00:03:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,29 +7,3 @@ class Dog
     puts "The Dog is sitting"
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Dog
-#
-#   def bark
-#     puts "Woof!"
-#   end
-#
-#   def sit
-#     puts "The Dog is sitting"
-#   end
-# end
